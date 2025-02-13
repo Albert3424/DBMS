7cf475dcc62b938af90661931dce9cbd7e9c8bcd
Hanzo Hasashi <Izukudeku09@outlook.com>
2025-02-13 23:37:28 +0500

    INSERT/SELECT Schedule DONE

--- T-SQL/SQLQuery1 - Schedule.sql
+++ T-SQL/SQLQuery1 - Schedule.sql
@@ -3,14 +3,14 @@ GO
 
 SET DATEFIRST 1;
 
-DECLARE @group				AS INT			=	(SELECT group_id			FROM Groups				WHERE group_name = N'PV_319');
-DECLARE @discipline			AS SMALLINT		=	(SELECT discipline_id		FROM Disciplines		WHERE discipline_name LIKE N'Обьектно-ориентированное программирование%')
-DECLARE @teacher			AS INT			=	(SELECT teacher_id			FROM Teachers		    WHERE first_name = N'Олег');
-DECLARE @start_date			AS DATE			=	N'2024-06-01';
-DECLARE @date				AS DATE			=	@start_date;
-DECLARE @number_of_lessons	AS TINYINT		=	(SELECT number_of_lessons	FROM Disciplines		WHERE discipline_id = @discipline);
-DECLARE @lesson				AS TINYINT		=	1;
-DECLARE @time				AS TIME			=	N'18:30';
+DECLARE @group				AS	INT			=	(SELECT group_id			FROM Groups			WHERE group_name = N'PV_319');
+DECLARE	@discipline			AS	SMALLINT	=	(SELECT discipline_id		FROM Disciplines	WHERE discipline_name LIKE N'????????-??????????????? ????????????????%');
+DECLARE @teacher			AS	SMALLINT	=	(SELECT teacher_id			FROM Teachers		WHERE first_name = N'????');
+DECLARE @start_date			AS	DATE		=	N'2024-06-01';
+DECLARE @date				AS	DATE		=	@start_date;
+DECLARE @number_of_lessons	AS	TINYINT		=	(SELECT number_of_lessons	FROM Disciplines	WHERE discipline_id=@discipline);
+DECLARE	@lesson				AS	TINYINT		=	1;
+DECLARE @time				AS	TIME(0)		=	N'18:30';
 
 WHILE (@lesson <= @number_of_lessons)
 BEGIN
@@ -18,23 +18,40 @@ BEGIN
 		PRINT(DATENAME(WEEKDAY, @date));
 		PRINT(@lesson);
 		PRINT(@time);
-		SET @lesson = @lesson + 1;
+
+		IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND discipline=@discipline AND [date]=@date AND [time]=@time)
+		BEGIN
+			INSERT Schedule
+					([group], discipline, teacher, [date], [time], spent)
+			VALUES	(@group, @discipline, @teacher, @date, @time, IIF(@date < GETDATE(), 1, 0));
+		END
+
+		SET	@lesson = @lesson+1;
 		PRINT(@lesson);
 		PRINT(DATEADD(MINUTE, 95, @time));
-		SET @lesson = @lesson + 1;
-		PRINT('--------------------------------------');
-		IF(DATEPART(WEEKDAY, @date) = 6)
+
+		IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND discipline=@discipline AND [date]=@date AND [time]=DATEADD(MINUTE, 95, @time))
 		BEGIN
-			SET @date = DATEADD(DAY, 3, @date);
+			INSERT	Schedule
+					([group], discipline, teacher, [date], [time], spent)
+			VALUES	(@group, @discipline, @teacher, @date, DATEADD(MINUTE, 95, @time), IIF(@date < GETDATE(), 1, 0));
+		END
+
+		SET	@lesson = @lesson+1;
+		PRINT('------------------------------------');
+		IF(DATEPART(WEEKDAY, @date)=6)
+		BEGIN
+			SET	@date = DATEADD(DAY, 3, @date);
 		END
 		ELSE
 		BEGIN
-			SET @date = DATEADD(DAY, 2, @date);
+			SET	@date = DATEADD(DAY, 2, @date);
 		END
 END
 
 --INSERT Schedule
---			([group], discipline, teacher, [date],		[time], spent)
+--		([group], discipline, teacher, [date],			[time], spent)
 --VALUES
---			(319,    1,			1,		N'2023-11-23', N'18:30',  1);
---
+--		(319,		1,			1,		N'2023-11-23', N'18:30', 1);
+
+SELECT * FROM Schedule;

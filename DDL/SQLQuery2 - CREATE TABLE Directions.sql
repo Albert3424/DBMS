8db21b921c264ed4772e352a5e22ed40b14eb230
Hanzo Hasashi <Izukudeku09@outlook.com>
2025-02-27 23:49:05 +0500

    TODO: Holidays

--- DDL/SQLQuery2 - CREATE TABLE Directions.sql
+++ DDL/SQLQuery2 - CREATE TABLE Directions.sql
@@ -1,7 +1,7 @@
-USE PV_319 ;
+USE PV_319_DDL;
 GO
 
-CREATE TABLE Direction
+CREATE TABLE Directions
 (
     direction_id    TINYINT          PRIMARY KEY,
 	direction_name  NVARCHAR(256)    NOT NULL

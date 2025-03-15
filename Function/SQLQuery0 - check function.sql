2ed9731f10a965311d826ffcf379c5d8d2bcd0fb
Hanzo Hasashi <Izukudeku09@outlook.com>
2025-03-15 22:56:44 +0500

    Dead end

--- Function/SQLQuery0 - check function.sql
+++ Function/SQLQuery0 - check function.sql
@@ -4,10 +4,25 @@ GO
 
 --PRINT(dbo.GetGroupID(N'PV_319'));
 --PRINT(dbo.GetDisciplineID(N'Hardware%'));
+----EXEC sp_PrintScheduleForGroup N'PV_319';
 --PRINT(dbo.CountSpentLessons(N'PV_319', N'Процедурное программирвоание'));
 --PRINT(dbo.GetNewYearHolidaysStartDate(2023));
 --PRINT(dbo.GetSummerHolidaysStart(2023));
---PRINT(dbo.GetHolidaysDuration(N'Летние%'))
+--PRINT(dbo.GetHolidaysDuration(N'Летние%'));
 
-PRINT('---------------------------');
-PRINT(dbo.GetHolidayStartDate(N'Майские', 2025));
+--PRINT('---------------------------');
+--PRINT(dbo.GetHolidayStartDate(N'Летние%', 2025));
+--PRINT(dbo.GetEasterDate(2004));
+--PRINT(dbo.GetLastDateForGroup(N'PV_319'));
+--DECLARE @date AS DATE = (SELECT MAX([date]) FROM Schedule WHERE [group]=dbo.GetGroupID(N'PV_319'));
+--PRINT(@date);
+
+DECLARE @group		AS NVARCHAR(10) = N'PV_319';
+DECLARE @date		AS DATE		=	dbo.GetLastDateForGroup(@group);
+WHILE   @date < '2025-01-01'
+BEGIN
+	PRINT (@date);
+	PRINT (DATENAME(WEEKDAY, @date));
+	PRINT ('----------------------');
+	SET	   @date = dbo.GetNextLearningDateFor(@group);
+END
--- T-SQL/SQLQuery2 - CREATE PROC ScheduleForStacionarGroup.sql
+++ T-SQL/SQLQuery2 - CREATE PROC ScheduleForStacionarGroup.sql
@@ -4,17 +4,18 @@ GO
 ALTER PROCEDURE dbo.sp_SetScheduleForStacionarGroup
 	@group_name			NVARCHAR(16),
 	@discipline_name	NVARCHAR(150),
-	@teacher_last_name	NVARCHAR(50),
-	@start_date			DATE,
-	@time				TIME(0)
+	@teacher_last_name	NVARCHAR(50)
 AS
 BEGIN
 	SET DATEFIRST 1;
 
+	DECLARE @start_date			AS  DATE		=	dbo.GetNextLearningDateFor(@group_name);
 	DECLARE @group				AS	INT			=	(SELECT group_id			FROM Groups			WHERE group_name = @group_name);
 	DECLARE	@discipline			AS	SMALLINT	=	(SELECT discipline_id		FROM Disciplines	WHERE discipline_name LIKE @discipline_name);
 	DECLARE @teacher			AS	SMALLINT	=	(SELECT teacher_id			FROM Teachers		WHERE last_name = @teacher_last_name);
 	DECLARE @date				AS	DATE		=	@start_date;
+	DECLARE @time				AS  TIME		=	(SELECT start_time FROM Groups WHERE group_name = @group_name);
+	
 	DECLARE @number_of_lessons	AS	TINYINT		=	(SELECT number_of_lessons	FROM Disciplines	WHERE discipline_id=@discipline);
 	DECLARE	@lesson				AS	TINYINT		=	1;
 	
@@ -27,7 +28,7 @@ BEGIN
 	
 			IF NOT EXISTS (SELECT [date] FROM DaysOFF WHERE [date]=@date)
 			BEGIN
-				--First lesson per day:
+
 				IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND [date]=@date AND [time]=@time)--AND discipline=@discipline 
 				BEGIN
 					INSERT Schedule
@@ -35,11 +36,10 @@ BEGIN
 					VALUES	(@group, @discipline, @teacher, @date, @time, IIF(@date < GETDATE(), 1, 0));
 					SET	@lesson = @lesson+1;
 				END
-		--		IIF(condition, value_1, value_2);
 	
 				PRINT(@lesson);
 				PRINT(DATEADD(MINUTE, 95, @time));
-				--Second lesson per day:
+
 				IF NOT EXISTS (SELECT * FROM Schedule WHERE [group]=@group AND [date]=@date AND [time]=DATEADD(MINUTE, 95, @time))--AND discipline=@discipline 
 				BEGIN
 					INSERT	Schedule
@@ -50,13 +50,14 @@ BEGIN
 	
 			END
 			PRINT('------------------------------------');
-			IF(DATEPART(WEEKDAY, @date)=6)
-			BEGIN
-				SET	@date = DATEADD(DAY, 3, @date);
-			END
-			ELSE
-			BEGIN
-				SET	@date = DATEADD(DAY, 2, @date);
-			END
+			--IF(DATEPART(WEEKDAY, @date)=6)
+			--BEGIN
+			--	SET	@date = DATEADD(DAY, 3, @date);
+			--END
+			--ELSE
+			--BEGIN
+			--	SET	@date = DATEADD(DAY, 2, @date);
+			--END
+			SET @date = dbo.GetNextLearningDateFor(@group_name);
 		END
 END
